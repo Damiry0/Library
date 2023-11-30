75ebfa52731d4e1f64a1582258df1553c1e008ee
Damian Ryś <damian.rys0@gmail.com>
2023-11-30 15:30:30 +0100

    refactor

---
+++ API/Migrations/20231124100500_UpdatedBookEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedBookEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Remaining",
+                table: "Books",
+                newName: "Amount");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublicationDate",
+                table: "Books",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "Books",
+                newName: "Remaining");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublicationDate",
+                table: "Books",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
