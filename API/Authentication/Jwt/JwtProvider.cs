e12ee37a1615b6a044ff3fe9a1271c05d9697022
Damian Ryś <damian.rys0@gmail.com>
2023-12-22 13:52:49 +0100

    Fixed problem with authorization encryption

--- API/Authentication/Jwt/JwtProvider.cs
+++ API/Authentication/Jwt/JwtProvider.cs
@@ -22,11 +22,12 @@ public class JwtProvider : IJwtProvider
         var claims = new Claim[]
         {
             new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
-            new(JwtRegisteredClaimNames.Email, user.Email),
+            new(JwtRegisteredClaimNames.Email, user.Email)
         };
 
-        var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secret-keys")),
-            SecurityAlgorithms.Sha256);
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)),
+            SecurityAlgorithms.HmacSha256);
         var token = new JwtSecurityToken(_jwtOptions.Issuer, _jwtOptions.Audience, claims, null,
             DateTime.UtcNow.AddHours(1),
             signingCredentials);
